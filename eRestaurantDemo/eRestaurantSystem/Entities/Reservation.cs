f4c9998ea0a610d68ef6b9153d6edc560faaf0c3
Col Walang <CWalang1@studentmail.nait.ca>
2015-09-21 17:07:08 -0600

    Setup of  Entity Samples

---
+++ eRestaurantDemo/eRestaurantSystem/Entities/Reservation.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+#endregion
+
+namespace eRestaurantSystem.Entities
+{
+    public class Reservation
+    {
+        [Key]
+        public int ReservationID { get; set; }
+        [Required(ErrorMessage = "A Customer Name is Required ")]
+        [StringLength(48, ErrorMessage = "Customer Name must be less than 48 characters")]
+        public string CustomerName { get; set; }
+        public DateTime ReservationDate { get; set; }
+        [Range(1,16, ErrorMessage="Party size is limited to 1-16 people")]
+        public int NumberInParty { get; set; }
+        [StringLength(15)]
+        public string ContactPhone { get; set; }
+        [Required,StringLength(1,MinimumLength=1, ErrorMessage="Reservation status is required")]
+        public string ReservationStatus { get; set; }
+        [StringLength(1)]
+        public string EventCode { get; set; }
+
+
+        //Navigation properties 
+        public virtual SpecialEvent Event { get; set; }
+        //The ReservationsTables is a many to many relationship to Tables table the sql
+        //ReservationsTable resovles this problem however ReservationTable holds only a compound primary key.
+        //We will NOT create a ReservationTable entity in our project but handle it via navigation mapping.
+        //Therefore we will place an ICollection property in this entity referring to the Tables table.
+        public virtual ICollection<Table> Tables { get; set; }
+
+
+    }
+}
---
+++ eRestaurantDemo/eRestaurantSystem/Entities/Table.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+#endregion
+
+namespace eRestaurantSystem.Entities
+{
+    public class Table
+    {
+        [Key]
+        public int TableID { get; set; }
+        [Required, Range(1,25)]
+        public byte TableNumber { get; set; }
+        public bool Smoking { get; set; }
+        public int Capacity { get; set; }
+        public bool Available { get; set; }
+
+        //Navigation property 
+        //The ReservationsTables is a many to many relationship to Tables table the sql
+        //ReservationsTable resovles this problem however ReservationTable holds only a compound primary key.
+        //We will NOT create a ReservationTable entity in our project but handle it via navigation mapping.
+        //Therefore we will place an ICollection property in this entity referring to the Reservation table.
+        public virtual ICollection<Reservation> Reservations { get; set; }
+
+        public Table()
+        {
+            Available = true;
+        }
+    }
+}
